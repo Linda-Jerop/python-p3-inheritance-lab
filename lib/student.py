a6f46be50f9c7587f9d74a81fa053a14752708f6
Linda <jeropkoimur@gmail.com>
2025-11-20 10:21:16 +0300

    inheritance

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,11 @@
 
 from user import User
 
-class Student(User):
+class Student(User):  # inherits from User class
     
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):  # initialize with first and last name
+        super().__init__(first_name, last_name)  # call parent class constructor
+        self.knowledge = []  # initialize with empty knowledge list
+    
+    def learn(self, new_knowledge):  # add string to knowledge list
+        self.knowledge.append(new_knowledge)  # append the new knowledge
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def __init__(self, first_name, last_name):  # initialize with first and last name
+        self.first_name = first_name  # save first name as attribute
+        self.last_name = last_name  # save last name as attribute
